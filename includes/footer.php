b1c29c2835433c06fc415a7706f92b15d429a8ad
Awais Rajpoot <107601883+Awais576@users.noreply.github.com>
2024-06-02 16:03:22 +0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Planet Shopify | All Rights Reserved | Contact Us: +92 90000 00000</b></span></div>
+    <div class="container text-center"><span class="text-muted"><b>Designed & Developed By Mr. Awais</b></span></div>
+</footer>
+    
